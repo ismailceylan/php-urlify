eafd2bf9f93d95495f397852aaa3fa733cf45530
Ismail Ceylan <ismaillceylan@gmail.com>
2025-04-13 18:08:53 +0300

    refactor: add base url class

---
+++ src/Url.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Iceylan\Urlify;
+
+use InvalidArgumentException;
+
+class Url
+{
+	/**
+	 * The original URL.
+	 * 
+	 * @var string
+	 */
+	private string $original;
+
+	/**
+	 * The options.
+	 *
+	 * @var integer
+	 */
+	private int $options;
+
+	/**
+	 * The parts of the URL.
+	 *
+	 * @var array
+	 */
+	private array $parts;
+
+	/**
+	 * Auto-detects the scheme of the URL.
+	 * 
+	 * @var int
+	 */
+	public const AutoDetectScheme = 1;
+
+	/**
+	 * Constructs a new Url object.
+	 *
+	 * @param string $url the URL to be parsed
+	 * @throws InvalidArgumentException if the given URL is invalid
+	 */
+	public function __construct( string $url, int $options = 0 )
+	{
+		$this->options = $options;
+		$this->original = $this->normalize( $url );
+		$this->parts = parse_url( $this->original );
+
+		$this->validate();
+	}
+
+	/**
+	 * Normalizes the given URL.
+	 *
+	 * If the given URL does not contain a scheme, and the Url::AutoDetectScheme
+	 * option is enabled, this method adds a scheme of "http://" to the URL.
+	 *
+	 * @param string $url the URL to be normalized
+	 * @return string the normalized URL
+	 */
+	private function normalize( string $url ): string
+	{
+		if( $this->options & self::AutoDetectScheme )
+		{
+			if( ! parse_url( $url, PHP_URL_SCHEME ))
+			{
+				$url = 'http://' . $url;
+			}
+		}
+
+		return $url;
+	}
+
+	/**
+	 * Validates the original URL.
+	 *
+	 * @throws InvalidArgumentException if the URL is invalid.
+	 */
+	protected function validate(): void
+	{
+		if( ! filter_var( $this->original, FILTER_VALIDATE_URL ) )
+		{
+			throw new InvalidArgumentException( "Invalid URL: {$this->original}" );
+		}
+	}
+
+}
